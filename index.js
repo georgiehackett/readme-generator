c52670a277a37bffdd4d12fd93e7aa51698a7f7f
Georgie <georgiehackett47@gmail.com>
2024-01-20 21:19:12 +0000

    Add array of questions for user prompts

--- index.js
+++ index.js
@@ -5,7 +5,62 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    // prompt for project title which will be displayed as the title of the README
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+    },
+    // prompt for description
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description for your project',
+    },
+    // prompt for installation instructions
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run to install dependencies?',
+    },
+    // prompt for usage information
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo?',
+    },
+    // prompt for license from a list of options - add a badge near top of README and add to info to license section
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license is your project covered under?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    // prompt for contributing
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to the repo?',
+    },
+    // prompt for tests section
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What command should be run to run tests?',
+    },
+    // prompt for questions section:
+    // user email with instructions on how to reach them
+    // GitHub username to generate a link to user's GitHub profile
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+    },
 ];
 
 // function to write README file
