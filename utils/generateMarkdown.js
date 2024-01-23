9026bb2821a0d2f2dc8a2bae748ffaa9e4a93a5b
Georgie <georgiehackett47@gmail.com>
2024-01-23 17:49:31 +0000

    Add user data to generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,35 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  1. [Description](#description)
+  2. [Installation](#installation)
+  3. [Usage](#usage)
+  4. [License](#license)
+  5. [Contributing](#contributing
+  6. [Tests](#tests)
+  7. [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  If you have any questions you can contact me at ${data.email} or ${data.username}
 `;
 }
 
