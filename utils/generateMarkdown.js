721827b485da790bff2a8e619590d811f49d8efb
Georgie <georgiehackett47@gmail.com>
2024-02-16 21:15:44 +0000

    Fix table of contents order when generating readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -43,9 +43,9 @@ function generateMarkdown(data) {
   
   ## Table of Contents
   1. [Description](#description)
-  2. [Installation](#installation)
-  3. [Usage](#usage)
-  4. [License](#license)
+  2. [License](#license)
+  3. [Installation](#installation)
+  4. [Usage](#usage)
   5. [Contributing](#contributing)
   6. [Tests](#tests)
   7. [Questions](#questions)
