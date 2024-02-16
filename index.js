2bf0d28ab91e9c353b5daf095a509d167a771366
Georgie <georgiehackett47@gmail.com>
2024-02-16 19:15:55 +0000

    Add badges to generateMarkdown

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-const licenseOptions = [
-  {
-    license: "MIT",
-    badge:
-      "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
-  },
-  {
-    license: "APACHE 2.0",
-    badge:
-      "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
-  },
-  {
-    license: "GPL 3.0",
-    badge:
-      "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
-  },
-  {
-    license: "BSD 3",
-    badge:
-      "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
-  },
-];
+// const licenseOptions = [
+//   {
+//     license: "MIT",
+//     badge:
+//       "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+//   },
+//   {
+//     license: "APACHE 2.0",
+//     badge:
+//       "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+//   },
+//   {
+//     license: "GPL 3.0",
+//     badge:
+//       "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+//   },
+//   {
+//     license: "BSD 3",
+//     badge:
+//       "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+//   },
+// ];
 
 const questions = [
   // prompt for project title which will be displayed as the title of the README
@@ -56,9 +56,9 @@ const questions = [
     type: "list",
     name: "license",
     message: "Which license is your project covered under?",
-    // choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
-    choices: licenseOptions.map(option => option.license),
-    badges: licenseOptions.map(option => option.badge)
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+    // choices: licenseOptions.map(option => option.license),
+    // badges: licenseOptions.map(option => option.badge)
   },
   // prompt for contributing
   {
@@ -92,10 +92,10 @@ const promptUser = () => {
   return inquirer.prompt(questions);
 };
 
+
 // function to write README file
 function writeToFile(fileName, data) {
   const readmeData = generateMarkdown(data);
-  console.log(readmeData);
 
   const filePath = path.join(__dirname, fileName);
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,40 @@
+const licenseOptions = [
+  {
+    license: "MIT",
+    badge:
+      "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+  },
+  {
+    license: "APACHE 2.0",
+    badge:
+      "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+  },
+  {
+    license: "GPL 3.0",
+    badge:
+      "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+  },
+  {
+    license: "BSD 3",
+    badge:
+      "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+  },
+];
+
+
 // function to generate markdown for README
 function generateMarkdown(data) {
-  console.log(data.license);
+
+  const [license] = licenseOptions.filter((license) => license.license === data.license);
+
   return `# ${data.title}
 
   ## Description
   ${data.description}
 
   ## License
-  ${data.license}
+  ${license.badge}  
+  This application is covered under the ${data.license} license.
 
   ## Table of Contents
   1. [Description](#description)
@@ -31,7 +58,7 @@ function generateMarkdown(data) {
   ${data.tests}
 
   ## Questions
-  If you have any questions you can contact me at ${data.email} or ${data.username}
+  If you have any questions you can contact me at ${data.email} or find me on [github](https://github.com/${data.username})
 `;
 }
 
